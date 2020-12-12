ef89211bb2e9ef363c059bcf614f171da1d3f0bb
Shellsea <elainesbarrera@gmail.com>
2020-12-11 16:55:04 -0800

    ADDED: helpful comments, parsed saved notes as JSON

--- db/routes/api-routes.js
+++ db/routes/api-routes.js
@@ -1,31 +1,34 @@
+// localhost:2020/api  <==== is the base
+
 const router = require("express").Router();
 const fs = require("fs")
 const index = fs.readFileSync("./public/index.html", "utf8")
 const notes = fs.readFileSync("./public/notes.html", "utf8")
-// const savedNotes = fs.readFile("./db/db.json", "utf8", (err, data) =>{
-//   if (err) throw err;
-//   res.json(data)
-// })
 
+// testing connection
 router.get("/", (req, res) => {
   res.json({
     msg: "success",
   });
 });
 
+// GET: saved notes "String"
 router.get("/api/notes", (req,res) =>{
   fs.readFile("./db/db.json", "utf8", (err, data) =>{
     if (err) throw err;
-    res.json(data)
+    res.send(JSON.parse(data))
   })
 })
 
+// GET: notes.html
 router.get("/notes", (req, res) => {
   res.json(notes)
 });
 
+// GET: index.html
 router.get("/*", (req, res) => {
   res.json(index)
 });
+
 
 module.exports = router;
