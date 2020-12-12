aaa7768f4eff3ad29cf3540f7590692f73425783
Shellsea <elainesbarrera@gmail.com>
2020-12-11 16:11:53 -0800

    GET: notes, and index.html as res.json string

--- db/routes/api-routes.js
+++ db/routes/api-routes.js
@@ -1,9 +1,20 @@
 const router = require("express").Router();
+const fs = require("fs")
+const index = fs.readFileSync("./public/index.html", "utf8")
+const notes = fs.readFileSync("./public/notes.html", "utf8")
 
 router.get("/", (req, res) => {
   res.json({
     msg: "success",
   });
+});
+
+router.get("/notes", (req, res) => {
+  res.json(notes)
+});
+
+router.get("/*", (req, res) => {
+  res.json(index)
 });
 
 module.exports = router;
