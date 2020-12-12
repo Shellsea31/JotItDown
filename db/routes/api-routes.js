3b348b996b57aec5eb21299a7de48273d48ee424
Shellsea <elainesbarrera@gmail.com>
2020-12-11 16:18:11 -0800

    GET: saved notes in db.json

--- db/routes/api-routes.js
+++ db/routes/api-routes.js
@@ -2,12 +2,23 @@ const router = require("express").Router();
 const fs = require("fs")
 const index = fs.readFileSync("./public/index.html", "utf8")
 const notes = fs.readFileSync("./public/notes.html", "utf8")
+// const savedNotes = fs.readFile("./db/db.json", "utf8", (err, data) =>{
+//   if (err) throw err;
+//   res.json(data)
+// })
 
 router.get("/", (req, res) => {
   res.json({
     msg: "success",
   });
 });
+
+router.get("/api/notes", (req,res) =>{
+  fs.readFile("./db/db.json", "utf8", (err, data) =>{
+    if (err) throw err;
+    res.json(data)
+  })
+})
 
 router.get("/notes", (req, res) => {
   res.json(notes)
