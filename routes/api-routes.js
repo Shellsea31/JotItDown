a1d325e1fafd31d1287932f6b96a0c3b751663ff
Shellsea <elainesbarrera@gmail.com>
2020-12-14 17:48:36 -0800

    began pushing to notes in post request

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,9 +1,9 @@
 // localhost:2020/api  <==== is the base
 
 const router = require("express").Router();
-const fs = require("fs")
-const index = fs.readFileSync("./public/index.html", "utf8")
-const notes = fs.readFileSync("./public/notes.html", "utf8")
+const fs = require("fs");
+const index = fs.readFileSync("./public/index.html", "utf8");
+const notes = fs.readFileSync("./public/notes.html", "utf8");
 
 // testing connection
 router.get("/", (req, res) => {
@@ -13,31 +13,36 @@ router.get("/", (req, res) => {
 });
 
 // GET: saved notes as object notation
-router.get("/api/notes", (req,res) =>{
-  fs.readFile("./db/db.json", "utf8", (err, data) =>{
+router.get("/api/notes", (req, res) => {
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
     if (err) throw err;
-    res.json(JSON.parse(data))
-  })
-})
+    res.json(JSON.parse(data));
+  });
+});
 
 // POST: new note
-router.post("/api/notes", (req,res) =>{
-  console.log(req.body)
-  res.send({msg: "success"})
-})
+router.post("/api/notes", (req, res) => {
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    if (err) throw err;
+    const notes = JSON.parse(data)
+
+    notes.push({
+      title: req.body.title,
+      text: req.body.text,
+    })
+    res.send({msg: "success"});
+  });
+  console.log(notes)
+});
 
 // GET: notes.html
 router.get("/notes", (req, res) => {
-  res.send(notes)
+  res.send(notes);
 });
 
 // GET: index.html
 router.get("/*", (req, res) => {
-  res.send(index)
+  res.send(index);
 });
-
-
-
-
 
 module.exports = router;
