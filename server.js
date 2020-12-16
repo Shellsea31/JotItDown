0f62f7b48a67f155635f04be1cf03556cd37a71e
Shellsea <elainesbarrera@gmail.com>
2020-12-16 15:40:10 -0800

    cleaned up

--- server.js
+++ server.js
@@ -3,11 +3,8 @@ const app = express();
 const PORT = 2020;
 const apiRoutes = require("./routes/api-routes");
 
-
-// parse received url as a string and use it to grab data
 app.use(express.urlencoded({ extended: true }));
 
-// parse json data
 app.use(express.json());
 
 // it delivers public (front end) folder to user
