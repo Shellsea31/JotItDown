139987e3f5a3588bab9fdc443ffc2121703511e3
Shellsea <elainesbarrera@gmail.com>
2020-12-16 15:38:32 -0800

    FIXED: activeNote title and text match

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,9 +1,5 @@
-// localhost:2020/ <==== is the base
-
 const router = require("express").Router();
 const fs = require("fs");
-// const index = fs.readFileSync("./public/index.html", "utf8");
-// const notes = fs.readFileSync("./public/notes.html", "utf8");
 const { nanoid } = require("nanoid");
 let id = nanoid(7);
 const path = require("path");
@@ -12,9 +8,7 @@ const path = require("path");
 router.get("/api/notes", (req, res) => {
   fs.readFile("./db/db.json", "utf8", (err, data) => {
     if (err) throw err;
-    // console.log(data);
     const parsedData = JSON.parse(data);
-    console.log(parsedData);
     res.send(parsedData);
   });
 });
@@ -25,13 +19,11 @@ router.post("/api/notes", (req, res) => {
     if (err) throw err;
     const notes = JSON.parse(data);
 
-    notes.push({
+    note.push({
       title: req.body.title,
       text: req.body.text,
       id: id,
     });
-
-    // console.log(notes);
 
     fs.writeFile("./db/db.json", JSON.stringify(notes), (err) => {
       if (err) return res.send("failed to add");
@@ -41,15 +33,14 @@ router.post("/api/notes", (req, res) => {
 });
 
 router.delete("/api/notes/:id", (req, res) => {
-  // let keep;
+
   fs.readFile("./db/db.json", "utf8", (err, data) => {
-    // console.log(data)
+
     let obj = JSON.parse(data);
     let keep = obj.filter((element) => element.id !== req.params.id);
-    console.log(keep);
+
     fs.writeFile("./db/db.json", JSON.stringify(keep), (err) => {
       if (err) throw err;
-      // res.json(keep);
     });
     res.sendStatus(200);
   });
