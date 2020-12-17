dfbde8379587706e7ea949ce501e8b54903caa80
Shellsea <elainesbarrera@gmail.com>
2020-12-16 16:17:31 -0800

    changed directories of html files

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -48,12 +48,12 @@ router.delete("/api/notes/:id", (req, res) => {
 
 // GET: notes.html
 router.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/notes.html"));
+  res.sendFile(path.join(__dirname, "../notes.html"));
 });
 
 // GET: index.html
 router.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/index.html"));
+  res.sendFile(path.join(__dirname, "../index.html"));
 });
 
 module.exports = router;
