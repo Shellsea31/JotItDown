e71e6419ce9653326e7347bd2e45e01a3c4d92d7
Shellsea <elainesbarrera@gmail.com>
2020-12-17 21:26:57 -0800

    process.env

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 2020;
+const PORT = process.env.PORT || 2020;
 const apiRoutes = require("./routes/api-routes");
 
 app.use(express.urlencoded({ extended: true }));
